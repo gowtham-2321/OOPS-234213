cac9e13313f9f89c9e3d71ab4e5cde7fa66bdb15
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-02-01 09:41:57 +0530

    constructors

---
+++ lab session 7/constructor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class X {
+public:
+    int i;
+    string Name;
+    X(){  // constructor is a function, the name of the constructor should be same as that of the class name, a constructor doesnt have any return type
+      i=100;
+      Name="Sai";
+  }
+};
+
+int main() {
+  X x1;
+  cout<<x1.i<<endl;
+  cout<<x1.Name<<endl;
+  return 0;   
+}
