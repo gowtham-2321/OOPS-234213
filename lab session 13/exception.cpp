10f2f974633a433e378fcc1ffaa01da75f85325e
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-03-19 11:49:13 +0530

    exception handling

---
+++ lab session 13/exception.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void checker(string name) {
+    try {
+        if (name == "admin") { 
+            throw name;  // Throwing string
+        }
+        cout << "The chosen username is: " << name << endl;
+    }
+    catch (const string& ex) {  // refer geeks for geeks, a bit confusing but still!!!
+        cout << "Enter a valid name (admin is already chosen)\n";
+    }
+}
+
+int main() {
+    string name;
+    cout << "Enter a username: ";
+    cin >> name;
+    checker(name);
+    return 0;
+}
