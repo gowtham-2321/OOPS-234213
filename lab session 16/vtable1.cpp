324cef4483d63938512eafc7d3b5af22bf2972b9
Gowtham Jegathesan <gowthamjega2005@gmail.com>
2025-03-25 14:23:34 +0530

    Update vtable1.cpp

--- lab session 16/vtable1.cpp
+++ lab session 16/vtable1.cpp
@@ -2,24 +2,22 @@
 #include <iostream>
 using namespace std;
 class nonvirtual{
-int x;
-public:
-void func()
-{
-}
+    int x;
+    public:
+        void func(){
+        }
 };
 
-class withvirtual
-{
-int x; public:
-virtual void func()
-{
-}
+class withvirtual{
+    int x;
+    public:
+        virtual void func(){
+        }
 };
 int main()
-{
-cout << sizeof( nonvirtual)<< endl;
-cout << sizeof( withvirtual);
-return 0;
+    {
+    cout << sizeof( nonvirtual)<< endl;
+    cout << sizeof( withvirtual);
+    return 0;
 }
 
